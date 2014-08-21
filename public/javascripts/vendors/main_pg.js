cc25e31e1e1b855f75b0714e51fd4642b24ddb75
tanoj <tanoj.rout@gmail.com>
2014-08-21 12:29:23 +0530

    Revert "Revert "QUIZ APPS""
    This reverts commit 3d30ec6801629ce271072ea51aba560dc997a15e.

---
+++ public/javascripts/vendors/main_pg.js
@@ -0,0 +1,8 @@
+quizApp.controller('main_pg',function($scope,$location){
+    $scope.message = "Hello AngularJS";
+
+        $scope.go = function (path){
+            $location.path(path);
+        }
+
+});
