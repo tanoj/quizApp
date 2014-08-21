3d30ec6801629ce271072ea51aba560dc997a15e
tanoj <tanoj.rout@gmail.com>
2014-08-21 11:58:07 +0530

    Revert "QUIZ APPS"
    This reverts commit cc412d75c45d9e7b6ac1d7d7a3818463a248f674.

--- README.md
+++
@@ -1,6 +0,0 @@
-Quiz_Model
-==========
-
-The MEAN Stack: MongoDB, ExpressJS, AngularJS and Node.js. And Bootrstrape ,d3.js for UI.
-
-
--- public/javascripts/vendors/main_pg.js
+++
@@ -1,8 +0,0 @@
-quizApp.controller('main_pg',function($scope,$location){
-    $scope.message = "Hello AngularJS";
-
-        $scope.go = function (path){
-            $location.path(path);
-        }
-
-});
